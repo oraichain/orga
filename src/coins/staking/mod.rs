69373799771416dcc72985337fddd5656d16eed0
ducphamle2 <ducphamle212@gmail.com>
2024-01-16 18:00:04 -0800

    add query pool & rename query validators

--- src/coins/staking/mod.rs
+++ src/coins/staking/mod.rs
@@ -1,5 +1,8 @@
 use super::pool::{Child as PoolChild, ChildMut as PoolChildMut};
-use super::{Address, Amount, Balance, Coin, Decimal, Give, Pool, Symbol, VersionedAddress};
+use super::{
+    Address, Amount, Balance, Coin, Decimal, Give, Pool, QueryStakingPoolResponse, StakingPoolInfo,
+    Symbol, VersionedAddress,
+};
 use crate::abci::{BeginBlock, EndBlock};
 use crate::collections::{Deque, Entry, EntryMap, Map};
 use crate::context::GetContext;
@@ -553,7 +556,18 @@ impl<S: Symbol> Staking<S> {
     }
 
     #[query]
-    pub fn all_validators(&self) -> Result<Vec<ValidatorQueryInfo>> {
+    pub fn pool(&self) -> Result<QueryStakingPoolResponse> {
+        let staked = self.staked()?;
+        Ok(QueryStakingPoolResponse {
+            pool: StakingPoolInfo {
+                not_bonded_tokens: Amount::new(0u64),
+                bonded_tokens: staked,
+            },
+        })
+    }
+
+    #[query]
+    pub fn validators(&self) -> Result<Vec<ValidatorQueryInfo>> {
         self.validators
             .iter()?
             .map(|entry| {
